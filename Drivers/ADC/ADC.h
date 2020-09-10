fe617b6863465e437a6c29eb78ca13080d5930c6
Yousif magdy <64543334+usefmagdy@users.noreply.github.com>
2020-09-10 03:57:39 +0200

    Add files via upload

---
+++ Drivers/ADC/ADC.h
@@ -0,0 +1,15 @@
+/*
+ * ADC.h
+ *
+ *  Created on: Nov 22, 2019
+ *      Author: youssif
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+//void adc_init(uint8 channel);
+//uint16 Adc_GET_result(void);
+void ADC_init(uint8 channel);
+uint16 ADC_GET_result(void);
+#endif /* ADC_H_ */
